62bab425845b86f068a95152a39b95eb4c20b18b
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-21 16:56:02 -0700

    Create 'dashboard' namespace

--- server/server.js
+++ server/server.js
@@ -13,18 +13,24 @@ var capture = io.of('/capture');
 capture.on('connection', function(socket){
   console.log('Socket connetion established.');
   ++stats.connections;
-  console.log('There are currently ' + stats.connections + ' connections.');
-  console.log('Socket ID:',socket.id);
+  // console.log('There are currently ' + stats.connections + ' connections.');
+  // console.log('Socket ID:',socket.id);
   socket.on('client-data', function (data) {
     socketData[socket.id] = data;
-    console.log(socketData);
+    // console.log(socketData);
     stats.aggConfidence += parseInt(data.range);
     stats.avgConfidence = stats.aggConfidence / stats.connections;
-    console.log(stats);
+    // console.log(stats);
   });
+
   socket.on('disconnect', function () {
     --stats.connections;
     delete socketData[socket.id];
   });
+});
+
+var dashboard = io.of('dashboard');
+dashboard.on('connection', function(socket) {
 
 });
+  
