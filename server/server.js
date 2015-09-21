8fd1c981ac9b13760ca4bbf2c7245470a2660926
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-21 16:37:55 -0700

    Set up 'capture' namespace. Create socketData and stats objects. stats object tracks holds number of connections, aggregate confidence and average confidence. Create a socket listender to detect disconnects.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,25 @@ var io = require('socket.io')(server);
 server.listen(8080);
 app.use('/', express.static(__dirname + '/../client'));
 
-io.on('connection', function(socket){
+var socketData = {};
+var stats = { connections: 0, aggConfidence: 0};
+
+var capture = io.of('/capture');
+capture.on('connection', function(socket){
   console.log('Socket connetion established.');
+  ++stats.connections;
+  console.log('There are currently ' + stats.connections + ' connections.');
+  console.log('Socket ID:',socket.id);
+  socket.on('client-data', function (data) {
+    socketData[socket.id] = data;
+    console.log(socketData);
+    stats.aggConfidence += parseInt(data.range);
+    stats.avgConfidence = stats.aggConfidence / stats.connections;
+    console.log(stats);
+  });
+  socket.on('disconnect', function () {
+    --stats.connections;
+    delete socketData[socket.id];
+  });
+
 });
