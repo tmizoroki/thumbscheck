1b9ba6b28e55eb3447d29ada53c22869068c129f
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-21 14:56:15 -0700

    Tweaked socket.io connection handler

--- client/main.js
+++ client/main.js
@@ -1,10 +1,12 @@
+
+
 var showValue = function (val) {
   document.getElementById('confidence').innerHTML = val;
 };
 
-var socket = io.connect('http://localhost:8080');
+var socket = io('http://localhost:8080');
 
-socket.on('news', function (data) {
-  console.log(data);
+socket.on('connect', function (data) {
+  console.log("connected");
   socket.emit('my other event', { my: 'data' });
 });
