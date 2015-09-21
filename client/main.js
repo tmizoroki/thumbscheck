b6bf6bdd40fd32df0ca23b36aeb53a9470fc02ec
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-21 13:18:07 -0700

    Define socket connection and add scaffolding for an emmiter

--- client/main.js
+++ client/main.js
@@ -1,3 +1,10 @@
 var showValue = function (val) {
   document.getElementById('confidence').innerHTML = val;
 };
+
+var socket = io.connect('http://localhost:8080');
+
+socket.on('news', function (data) {
+  console.log(data);
+  socket.emit('my other event', { my: 'data' });
+});
