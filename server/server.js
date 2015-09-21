25e08ff240024aa62fd6dc0ec44b68c0564aba61
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-21 14:57:29 -0700

    Server logs socket.io connection

--- server/server.js
+++ server/server.js
@@ -6,4 +6,6 @@ var io = require('socket.io')(server);
 server.listen(8080);
 app.use('/', express.static(__dirname + '/../client'));
 
-io.on('connection', function(){ /* … */ });
+io.on('connection', function(socket){
+  console.log('Socket connetion established.');
+});
