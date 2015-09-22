7ccab9b6dba64c081a3368d3f96f4085a13c5b5f
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-22 15:10:18 -0700

    Delete console logs

--- public/main.js
+++ public/main.js
@@ -5,13 +5,11 @@ socket.on('connect', function () {
   var data = {
     range: document.getElementById('slider').value
   };
-  // console.log(data);
   socket.emit('client-data', data);
 });
 
 var showValue = function (val) {
   document.getElementById('confidence').innerHTML = val;
   var data = { range: val };
-  // console.log(data);
   socket.emit('client-data', data);
 };
