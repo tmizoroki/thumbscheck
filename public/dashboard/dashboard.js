16b5ad22fa79f7e553e0db40e0adc8ad5a3d7fe0
Tomio Mizoroki <tmizoroki@gmail.com>
2015-09-22 15:09:35 -0700

    Make line graph functional

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -1,20 +1,30 @@
-var avgConfGauge = $('#gaugeChart').epoch({
-  type: 'time.gauge',
-  format: function(v) { return (v * 100).toFixed(1) + '%'; }
-});
+$( function() {
+  //Average Confidence Gauge
+  var avgConfGauge = $('#gaugeChart').epoch({
+    type: 'time.gauge',
+    format: function(v) { return (v * 100).toFixed(1) + '%'; }
+  });
 
-var lineChartData = [{ label: 'avg', values: [] }];
-var avgConfLine = $('#lineChart').epoch({
-  type: 'time.line',
-  data: lineChartData
-});
+  var lineGraph = $('#lineGraph').epoch( {
+      type: 'time.line', axes: ['left', 'bottom', 'right'],
+      data: [ { values: [ { time: Date.now()/1000, y: 0 } ] } ],
+    } );
 
-var dashboard = io('http://localhost:8080/dashboard');
-dashboard.on('updateStats', function(refresh) {
-  console.log(refresh.avgConfidence);
+  // var heatMap = $('#heatmapChart').epoch({
+  //   type: 'time.heatmap',
+  //   data: [ { values: [ { time: Date.now()/1000 } ] } ],
+  //   bucketRange: [0, 100],
+  //   buckets: 20
+  // });
 
-  avgConfGauge.update(refresh.avgConfidence / 100);
-  // lineChartData[0].values.push({ time: Date.now()/1000, y: refresh.avgConfidence });
+  // console.log(heatMap.data);
 
 
+  var dashboard = io('http://localhost:8080/dashboard');
+  dashboard.on('updateStats', function(refresh) {
+    avgConfGauge.update(refresh.avgConfidence / 100);
+    // heatMap.data[0].values.push({ time: Date.now()/1000, histogram: refresh.histogram });
+    // console.log(heatMap.data);
+    lineGraph.push( [ { time: Date.now()/1000, y: refresh.avgConfidence } ] );
+  });
 });
